d97fd2b09b557151d237909fad6d6d0d79df9642
Christopher Benedict <chrisjbenedict@gmail.com>
2018-05-16 13:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.querySelector('#nested .target')
 }
 
-function increaseRankBy() {
+function increaseRankBy(n) {
   const rankedList = document.querySelectorAll('u1.ranked-list li')
   for (let i = 0; i < rankedList.length; i++) {
     return rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n
